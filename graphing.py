862807050b97f49ede4126a8be7985cccae6d84c
WhitingJarod <WhitingJarod@Gmail.com>
2023-02-28 20:17:08 -0700

    Functioning graphical plotter

--- graphing.py
+++ graphing.py
@@ -1,11 +1,14 @@
 from Expression import *
 from postfix import parsePostfix
 import math
+import pygame
+
+pygame.init()
 
 def map(value, from_min, from_max, to_min, to_max):
     return to_min + (to_max-to_min) * (value-from_min) / (from_max-from_min)
 
-def calculateDataset(expr: Expression, min_x: float, max_x: float, width: int, height: int):
+def calculateFunctionDataset(expr: Expression, min_x: float, max_x: float, width: int, height: int):
     data = []
     for pos_x in range(width):
         x = map(pos_x, 0, width-1, min_x, max_x)
@@ -18,10 +21,34 @@ def calculateDataset(expr: Expression, min_x: float, max_x: float, width: int, h
         data[pos] = (data[pos][0], data[pos][1] * ratio)
     return data
 
+def renderFunction(expr: Expression, min_x: float, max_x: float, width: int, height: int):
+    data = calculateFunctionDataset(expr, min_x, max_x, width, height)
+    surface = pygame.display.set_mode((width, height))
+    surface.fill((40, 42, 54))
+    best = (0, 100000)
+    for x in range(width):
+        number = abs(data[x][0])
+        if number < best[1]:
+            best = (x, number)
+    half_height = height/2
+    pygame.draw.line(surface, (0xbd, 0x93, 0xf9), (best[0], 0), (best[0], height))
+    pygame.draw.line(surface, (0xbd, 0x93, 0xf9), (0, half_height), (width, half_height))
+    for x in range(width):
+        pygame.draw.rect(surface, (0x39,0xed,0x7b), (x, -data[x][1]+half_height, 1, 1))
+    pygame.display.flip()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                break
+    pygame.display.quit()
+
 def main():
-    (expr, _) = parsePostfix("(x)(x)(x)")
-    result = calculateDataset(expr, 0, 10, 21, 15)
-    for val in result:
-        print(val)
+    (expr, _) = parsePostfix("|x|+1")
+    renderFunction(expr, -50, 50, 1920, 1080)
+    # result = calculateFunctionDataset(expr, -10, 10, 21, 15)
+    # for val in result:
+    #     print(val)
 
 if __name__ == "__main__": main()
--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-from Expression import *
 from postfix import parsePostfix
+from graphing import renderFunction
 
 def main():
+    print("\n\nWelcome to the function grapher.\n")
     while True:
-        text = input("Enter an expression.\n")
-        (result, var_names) = parsePostfix(text)
-        variables = {}
-        for name in var_names:
-            if name in variables: continue
-            value = input(f"(Optional) What is the value of {name}? ").strip()
-            if value != "":
-                variables[name] = value
-        print(f"The answer is {result(variables)}\n")
+        print("--------------------------------------------------------------------------------")
+        print("\nYou may use the following operations:")
+        print("a+b, a-b, a*b, a/b, a%b, a^b, -a, (a+b), |a+b|")
+        print("-(x), -|x|, (x)(x), (x)|x|, |x||x|, 3x, x2, 5x2")
+        expr = None
+        while True:
+            text = input("\nEnter a function expression using the variable x. (or 'q' to quit)\n")
+            if text.strip() == "q":
+                return
+            try:
+                expr = parsePostfix(text)
+                for name in expr[1]:
+                    if name != 'x':
+                        print(f"Invalid variable: {name}")
+                        continue
+                float(expr[0]({"x": 1}))
+                break
+            except:
+                print("Unable to parse the expression!")
+                continue
+        min_x = None
+        while True:
+            text = input("\nEnter the minimum X value to render. (or blank for -10)\n")
+            if text.strip() == "":
+                min_x = -10
+                break
+            try:
+                min_x = float(text.strip())
+                break
+            except:
+                print("Unable to parse the minimum X value!")
+                continue
+        max_x = None
+        while True:
+            text = input("\nEnter the maximum X value to render. (or blank for 10)\n")
+            if text.strip() == "":
+                max_x = 10
+                break
+            try:
+                max_x = float(text.strip())
+                break
+            except:
+                print("Unable to parse the maximum X value!")
+                continue
+        renderFunction(expr[0], min_x, max_x, 1920, 1080)
+
 
 if __name__ == "__main__": main()
